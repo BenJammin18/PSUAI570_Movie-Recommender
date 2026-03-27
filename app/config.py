2f39d2812dcbcd3f044f471ac2640d2ed884eed9
Benjamin Reber <ben.reber18@gmail.com>
2026-03-27 15:13:19 -0400

    update data path

--- app/config.py
+++ app/config.py
@@ -3,5 +3,5 @@ from pathlib import Path
 ROOT = Path(__file__).resolve().parent.parent
 DATA_DIR = ROOT / 'data'
 MODELS_DIR = ROOT / 'models'
-DEFAULT_DATASET_PATH = DATA_DIR / 'movies.csv'
+DEFAULT_DATASET_PATH = DATA_DIR / 'movie_data.csv'
 DEFAULT_EMBEDDINGS_PATH = MODELS_DIR / 'movie_features.pkl'
